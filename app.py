fdc66e0d0bc2971308a51a47e581ea8a41932d1a
Gerson Aranibar <aranibargerson28@gmail.com>
2021-10-27 00:49:04 -0500

    update files

--- app.py
+++ app.py
@@ -2,24 +2,59 @@ from flask import Flask
 from flask import render_template
 from flask import send_file
 from flaskext.mysql import MySQL
-
+from flask import request
 mysql = MySQL(host="localhost", user="root", password="", db="movies")
 app = Flask(__name__)
 mysql.init_app(app)
 conn = mysql.connect()
 
+def prepare_data(movies):
+
+    final_movies = []
+
+    q = """SELECT mg.idmovie, g.title FROM moviegenre AS mg 
+    LEFT JOIN genre AS g 
+    ON mg.idgenre = g.idgenre 
+    WHERE idmovie = """
+
+    for movie in movies:
+        cursor = conn.cursor()
+        query = q + str(movie[0])
+        cursor.execute(query)
+        results = cursor.fetchall()
+        genres = list(map(lambda x: x[1], results))
+        modified_movie = list(movie)
+        modified_movie.append(", ".join(genres))
+        final_movies.append(modified_movie)
+    return final_movies
+
 @app.route('/')
 def index():
     return("Hello Wolrd!")
 
+# /movies/from/2016/to/2018
+@app.route("/movies/from/<begin>/to/<end>")
+def movies_by_range(begin, end):
+    filter = f"WHERE year BETWEEN {begin} AND {end}"
+    pass
+
 @app.route('/movies')
 def list_movies():
+
+    q = request.args.get('q')
+
     cursor = conn.cursor()
-    query = "SELECT * FROM movie"
+    query = "SELECT * FROM movie "
+
+    if q != None:
+        query = query + f"WHERE title LIKE '%{q}%' " 
+        query = query + f"OR director LIKE '%{q}%' " 
+        query = query + f"OR plot LIKE '%{q}%'" 
+        
     cursor.execute(query)
     results = cursor.fetchall()
-    print (results)
-    return render_template("index.html", movies = results)
+    data = prepare_data(results)
+    return render_template("index.html", movies = data)
 
 @app.route('/styles.css')
 def styles():
--- data.sql
+++ data.sql
@@ -35,6 +35,22 @@ VALUES
 "https://movieplayer.net-cdn.it/t/images/2017/12/20/bright_jpg_191x283_crop_q85.jpg",
 "#0d0d0c");
 
+INSERT INTO movie (`title`, `plot`, `year`, `length`, `director`, `background`, `coverimage`, `maincolor`)
+VALUES 
+("Tomb Raider", "Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared.",
+2018, 125, "Roar Uthaug", 
+"https://fsmedia.imgix.net/cd/c9/5e/ba/4817/4d9a/93f0/c776ec32ecbc/lara-crofts-neck-looks-unnatural-in-the-new-poster-for-tomb-raider.png",
+"https://mr.comingsoon.it/imgdb/locandine/235x336/53750.jpg",
+"#0d0d0c");
+
+INSERT INTO movie (`title`, `plot`, `year`, `length`, `director`, `background`, `coverimage`, `maincolor`)
+VALUES 
+("Black Panther", "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
+2018, 134, "Ryan Coogler", 
+"https://www.gannett-cdn.com/-mm-/c03fd140debe8ad4c05cf81a5cad7ad61a12ce52/c=0-1580-2985-3266&r=x803&c=1600x800/local/-/media/2017/06/09/USATODAY/USATODAY/636326272873599176-Black-Panther-Teaser.jpg",
+"https://mr.comingsoon.it/imgdb/locandine/235x336/53715.jpg",
+"#0d0d0c");
+
 INSERT INTO `movies`.`genre` (`title`) VALUES ("Action");
 INSERT INTO `movies`.`genre` (`title`) VALUES ("Crime");
 INSERT INTO `movies`.`genre` (`title`) VALUES ("Fantasy");
@@ -44,3 +60,13 @@ INSERT INTO `movies`.`genre` (`title`) VALUES ("Sci-Fi");
 INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("1", "1");
 INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("1", "2");
 INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("1", "3");
+INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("2", "1");
+INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("2", "3");
+INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("3", "1");
+INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("3", "4");
+INSERT INTO `movies`.`moviegenre` (`idmovie`,`idgenre`) VALUES("3", "3");
+
+SELECT mg.idmovie, g.title FROM moviegenre AS mg
+LEFT JOIN genre AS g
+ON mg.idgenre = g.idgenre
+WHERE idmovie = 2
